3e03cb0ba64b1811af8ccd2fd6a9ab6418e03a4f
austinwoo123 <austinwoo123@gmail.com>
2021-10-08 12:32:44 -0700

    add starting file for backend

---
+++ Feedback-Central-Backend/src/test/java/com/company/FeedbackCentral/FeedbackCentralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.FeedbackCentral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackCentralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
