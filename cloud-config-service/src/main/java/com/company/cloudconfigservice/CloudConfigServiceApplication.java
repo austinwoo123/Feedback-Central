e843d3431ae27f691ecb9829a859cb70add27f44
austinwoo123 <austinwoo123@gmail.com>
2021-10-11 07:58:59 -0700

    add eureka service

--- cloud-config-service/src/main/java/com/company/cloudconfigservice/CloudConfigServiceApplication.java
+++ cloud-config-service/src/main/java/com/company/cloudconfigservice/CloudConfigServiceApplication.java
@@ -2,9 +2,10 @@ package com.company.cloudconfigservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@Enable
+@EnableConfigServer
 public class CloudConfigServiceApplication {
 
 	public static void main(String[] args) {
