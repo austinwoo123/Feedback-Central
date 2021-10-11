367f26167cd8ead4b222937e0654c21a6b50aa59
austinwoo123 <austinwoo123@gmail.com>
2021-10-11 07:38:53 -0700

    add cloud config service

---
+++ cloud-config-service/src/main/java/com/company/cloudconfigservice/CloudConfigServiceApplication.java
@@ -0,0 +1,14 @@
+package com.company.cloudconfigservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Enable
+public class CloudConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudConfigServiceApplication.class, args);
+	}
+
+}
