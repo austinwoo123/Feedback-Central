29e23a97dc8dc3224f4fbcc5260cbe7c4d40768b
chavalk <chavalk@hotmail.com>
2021-10-14 13:47:38 -0500

    Add service layer and view model.

--- employee-service/src/main/java/com/company/employeeservice/dao/NotesRepository.java
+++ employee-service/src/main/java/com/company/employeeservice/dao/NotesRepository.java
@@ -4,7 +4,10 @@ import com.company.employeeservice.dto.Notes;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface NotesRepository extends JpaRepository<Notes, Integer> {
 
+    List<Notes> findByEmployeeId(Integer id);
 }
---
+++ employee-service/src/main/java/com/company/employeeservice/viewmodel/EmployeeViewModel.java
@@ -0,0 +1,69 @@
+package com.company.employeeservice.viewmodel;
+
+import com.company.employeeservice.dto.Notes;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class EmployeeViewModel {
+
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String department;
+    private List<Notes> notes = new ArrayList<>();
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public List<Notes> getNotes() {
+        return notes;
+    }
+
+    public void setNotes(List<Notes> notes) {
+        this.notes = notes;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EmployeeViewModel that = (EmployeeViewModel) o;
+        return Objects.equals(id, that.id) && Objects.equals(firstName, that.firstName) && Objects.equals(lastName, that.lastName) && Objects.equals(department, that.department) && Objects.equals(notes, that.notes);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, department, notes);
+    }
+}
