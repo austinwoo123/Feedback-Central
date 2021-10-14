7bd4c7bf8fc96a3474e1c82591cd8b527bea2713
marcuspramirez <@macusramirez@gmail.com>
2021-10-14 09:43:27 -0500

    links fixed

--- feedback-central/src/App.js
+++ feedback-central/src/App.js
@@ -3,7 +3,7 @@ import { DarkModeSwitch } from "./components/DarkModeSwitch";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import LoginPage from "./components/pages/LoginPage";
 import Home from "./components/pages/Home";
-import About from "./components/pages/About";
+import Landing from "./components/pages/Landing";
 import View from "./components/pages/View";
 import Contact from "./components/pages/Contact";
 import { QueryClient, QueryClientProvider } from "react-query";
@@ -15,9 +15,10 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <Router>
         <Switch>
-          <Route path="/" exact component={About} />
+          <Route path="/" exact component={Landing} />
+          <Route path="/Home" component={Home} />
           <Route path="/Login" component={LoginPage} />
-          <Route path="/About" component={About} />
+          <Route path="/Home" component={Home} />
           <Route path="/Contact" component={Contact} />
           <Route path="/View" component={View} />
         </Switch>
