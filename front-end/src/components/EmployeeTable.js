6311e60f27db09eb54621b2f8f903eaeaac1328d
marcuspramirez <@macusramirez@gmail.com>
2021-10-15 14:32:44 -0500

    ready to push

--- front-end/src/components/EmployeeTable.js
+++ front-end/src/components/EmployeeTable.js
@@ -1,8 +1,5 @@
 import React from "react";
-<<<<<<< HEAD
-=======
 import { DeleteIcon } from "@chakra-ui/icons";
->>>>>>> f38ab99963871bf68b7d1a9a7acd1b732b0a5b29
 import { Link } from "react-router-dom";
 import {
   Table,
@@ -68,12 +65,9 @@ class EmployeeTable extends React.Component {
             <Th>First Name</Th>
             <Th>Last Name</Th>
             <Th>Department</Th>
-<<<<<<< HEAD
-=======
             <Th>Postive Notes</Th>
             <Th>Needs Improvement</Th>
             <Th>Other Notes</Th>
->>>>>>> f38ab99963871bf68b7d1a9a7acd1b732b0a5b29
           </Tr>
         </Thead>
         <Tbody>
@@ -83,13 +77,10 @@ class EmployeeTable extends React.Component {
               <Td>{Employee.firstName}</Td>
               <Td>{Employee.lastName}</Td>
               <Td>{Employee.department}</Td>
-<<<<<<< HEAD
-=======
               <Td>{Employee.positives}</Td>
               <Td>{Employee.needImprovement}</Td>
               <Td>{Employee.note}</Td>
 
->>>>>>> f38ab99963871bf68b7d1a9a7acd1b732b0a5b29
               <Td>
                 <Button colorScheme="teal">
                   <Link
@@ -101,8 +92,6 @@ class EmployeeTable extends React.Component {
                   </Link>
                 </Button>
               </Td>
-<<<<<<< HEAD
-=======
               <Td>
                 <Button
                   leftIcon={<DeleteIcon />}
@@ -116,7 +105,6 @@ class EmployeeTable extends React.Component {
                   Delete 🔥
                 </Button>
               </Td>
->>>>>>> f38ab99963871bf68b7d1a9a7acd1b732b0a5b29
             </Tr>
           ))}
         </Tbody>
