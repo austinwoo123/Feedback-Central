71d904ab0468c79972483491dbbc4f7a1c261725
Captain <71038018+karimi65@users.noreply.github.com>
2021-10-18 10:56:30 -0500

    update background color

--- front-end/src/components/navbar/index.jsx
+++ front-end/src/components/navbar/index.jsx
@@ -12,6 +12,7 @@ const NavbarContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  background-color: #00b5ad;
 `;
 
 const BrandContainer = styled.div``;
